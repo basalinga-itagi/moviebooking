0e45450943cc388b44cdadce104f0763406524b6
basalingappa <basalingappa.itagii@gmail.com>
2023-10-20 08:57:11 +0530

    implementing login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { signUp } from "../controllers/authController.js";
+import { signUp,signIn } from "../controllers/authController.js";
 
 const router = express.Router();
 
+router.post("/login", signIn);
 router.post("/signup", signUp);
+
+
 export default router;
