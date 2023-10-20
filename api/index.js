c042bd904c163495b20048032c8ef240665c6039
user <basalingappa.itagi@inqude.com>
2023-10-20 09:55:18 +0530

    implementing basic setup for backnd

--- api/index.js
+++ api/index.js
@@ -1,10 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGOURL);
+    console.log("Connected to Mongo");
+  } catch (e) {
+    console.log("error while connectig to mongo db");
+  }
+};
 
 app.use("/", (req, res) => {
   res.send("Hello");
 });
 
 app.listen(8088, () => {
-  console.log("connected to db");
+  console.log("connected to port server");
+  connectToDb();
 });
